736fbce11007d9e12e204248a3025e8e4e5a0f05
Hyemin Park <85723134+hyemin085@users.noreply.github.com>
2021-09-26 11:07:23 +0900

    [백준/node.js] 알고리즘

---
+++ 2021.09/backjoon_나머지_3052.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+// const filepath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+// let input = fs.readFileSync(filepath).toString().split("\n").map(val=>+val);
+
+const input = fs.readFileSync('dev/stdin').toString().trim().split('\n')
+
+let Array = input.map((input) => input % 42);
+let set = new Set(Array);
+const Arr = [...set];
+
+console.log(Arr.length);
