5c35233c4dc9159f88f5410b0c7f957571a7e159
Hyemin Park <85723134+hyemin085@users.noreply.github.com>
2021-10-27 12:10:23 +0900

    [알고리즘] 2021.10

---
+++ 2021.10/baekjoon_ACM 호텔_10250/index.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+let num = input[0]
+
+let hotel = input.splice(1);
+
+
+
+let floor = 0;
+let ho = 0;
+for(let i = 0; i < num; i++) {
+  let [h, w, n] = hotel[i].split(" ");
+
+  if(n % h === 0) {
+    floor = h *100;
+    ho = n / h;
+  }
+  else {
+    floor = (n%h) * 100;
+    ho = 1 + n / h
+  }
+  console.log(Math.floor(floor+ho));
+}
+
---
+++ 2021.10/baekjoon_달팽이는 올라가고 싶다_2869/index.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+let Num = input[0].split(" ");
+
+let A = Number(Num[0]);
+let B = Number(Num[1]);
+let V = Number(Num[2]);
+
+
+console.log(Math.ceil((V - B) / (A - B)));
