933b22b91973423b35c78be5c7d9e5f43691b131
Hyemin Park <85723134+hyemin085@users.noreply.github.com>
2021-11-02 00:51:20 +0900

    [알고리즘] 2021.11.02 소수

---
+++ 2021.11/baekjoon_소수찾기_1978/baekjoon_소수_2581/index.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+
+const M = Number(input[0]);
+const N = Number(input[1]);
+
+const rangeNum = [1];
+
+for (let i = M; i <= N; i++){
+  rangeNum.push(i);
+}
+
+const isPrime = (x) => {
+  if (x < 2) {
+    return false;
+  }
+  for (let i = 2; i * i <= x; i++) {
+    if (x % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+let total = 0;
+let min = [];
+rangeNum.map((x) => {
+  if (isPrime(x)) {
+    min.push(x);
+    total += x;
+  }
+})
+
+if (min[0]>0 || total >0) {
+  console.log(total);
+  console.log(min[0]);
+}else{
+  console.log(-1);
+}
