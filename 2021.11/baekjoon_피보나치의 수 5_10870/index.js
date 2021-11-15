7a469ba192cfaae92d7bceeda8d0780cac375f25
헹구 <hyemin085@gmail.com>
2021-11-15 14:46:17 +0900

    [알고리즘] 2021.11.15.

--- 2021.11/baekjoon_피보나치의 수 5_10870/index.js
+++ 2021.11/baekjoon_피보나치의 수 5_10870/index.js
@@ -4,8 +4,8 @@ let input = fs.readFileSync(filePath).toString().trim().split("\n")
 
 let n = +input;
 
-let pibo = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765];
+let fibo = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765];
 
 
-console.log(pibo[n]);
+console.log(fibo[n]);
 
