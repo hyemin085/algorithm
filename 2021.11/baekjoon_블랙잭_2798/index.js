31463df6b406e25ccff6fab762dfc0612067dec8
헹구 <hyemin085@gmail.com>
2021-11-25 11:49:10 +0900

    [알고리즘] 2021.11.25.

---
+++ 2021.11/baekjoon_블랙잭_2798/index.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const first = input[0].split(" ")
+const second = input[1].split(" ").map(el => parseInt(el))
+
+const N = parseInt(first[0])
+const M = parseInt(first[1])
+
+let max = 0
+
+for (let i = 0; i < N; i++) {
+  for (let j = 0; j < N; j++) {
+    for (let k = 0; k < N; k++) {
+
+      if (i == j || i == k || j == k) {
+        continue;
+      }
+
+
+      const sum = second[i] + second[j] + second[k]
+      if (sum > max && sum <= M) {
+        max = sum
+      }
+
+      if (max == M) {
+        break;
+      }
+    }
+  }
+}
+
+console.log(max)
