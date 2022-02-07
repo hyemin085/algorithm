7c23b93b3f006a79919dbb20b3402795e1a0f8db
헹구 <hyemin085@gmail.com>
2022-02-07 10:55:59 +0900

    [알고리즘]

---
+++ 2022.02/baekjoon_2750_수 정렬하기/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const N = input.shift();
+
+const sorted = input.sort((a,b) => a-b);
+
+for(let i = 0; i < N; i++){
+  console.log(sorted[i]);
+}
+
