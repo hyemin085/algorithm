b4352dfe4e9a11bc52cc1f7cd7e930f599c54b24
헹구 <hyemin085@gmail.com>
2022-04-01 23:18:02 +0900

    🔥 알고리즘

--- 2022.04/baekjoon_24883_자동완성/index.js
+++ 2022.04/baekjoon_24883_자동완성/index.js
@@ -1,12 +1,21 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().split("\n");
+// const fs = require('fs');
+//
+// const input = fs.readFileSync("/dev/stdin").toString().trim();
 
-let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const alpa = function (word) {
-  if(word.toLowerCase() === 'n'){
-    console.log("Naver D2");
-  }else console.log("Naver Whale");
+
+const word = input[0];
+
+
+const alpa = function (words) {
+  words.toLowerCase() === 'n'?   console.log("Naver D2") : console.log("Naver Whale")
+
+
 }
 
 
-alpa(input);
+alpa(word);
 
